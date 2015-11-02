245f183be89e674b3a3789d8782e5c19de8d7d9b
emmanuel <emmanuel.rudelle@hotmail.fr>
2015-11-02 18:23:04 +0100

    added validations and relations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ActiveRecord::Base
   belongs_to :renter, :class_name => :User,:foreign_key => "user_id"
   belongs_to :cat
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+
 end
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,3 +1,10 @@
 class Cat < ActiveRecord::Base
   belongs_to :owner, :class_name => :User,:foreign_key => "user_id"
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :gender, presence: true
+  validates_inclusion_of :gender, :in => ["male","female"]
+  validates :zip_code, presence: true
+  validates :city, presence: true
+  validates :address, presence: true
 end
