cf2d01639210ed5bfc35701b11406ecf40c88f68
emmanuel <emmanuel.rudelle@hotmail.fr>
2015-11-03 16:44:05 +0100

    added details in booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,25 @@
 class BookingsController < ApplicationController
-
-
-  def index
-  @bookings = Booking.all
-  end
+ before_action :authenticate_user!
+ skip_before_action :authenticate_user!, only: [ :show, :create]
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def create
+    @cat = Cat.find(params[:id])
+    @booking = @cat.bookings.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+       render :new
+     end
   end
+
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :cat_id, :renting_price, :start_time, :end_time)
+  end
+
 
 end
